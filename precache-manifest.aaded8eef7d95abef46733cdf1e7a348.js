64783daf57cba8455f1021bc1725360a0d849c58
himanshusharma6797 <himanshusharma06071997@gmail.com>
2022-04-23 12:05:08 +0530

    Updates

---
+++ precache-manifest.aaded8eef7d95abef46733cdf1e7a348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b79397663ceaba23e4b",
+    "url": "/TextUtils/static/css/main.ef1e3b4b.chunk.css"
+  },
+  {
+    "revision": "6b79397663ceaba23e4b",
+    "url": "/TextUtils/static/js/main.6b793976.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/TextUtils/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "ae1ab5972405ba779003",
+    "url": "/TextUtils/static/js/runtime~main.ae1ab597.js"
+  },
+  {
+    "revision": "dca70c8de50b36261a79d6a1f46be99e",
+    "url": "/TextUtils/index.html"
+  }
+];
